d4ba1989425c7d269785959f1f0354735062e734
Steven <steven950811@gmail.com>
2025-02-13 00:30:40 +0800

    fix: fix the wrong calculation of cost

--- arbitrage-trade/main.py
+++ arbitrage-trade/main.py
@@ -7,4 +7,4 @@ dotenv.load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 dc_bot = DiscordBot(os.getenv("DCBOT_TOKEN"), os.getenv("CHANNEL_ID"))
-dc_bot.run("1101.TW", "DFF")
+dc_bot.run("1101.TW", "DFF", 25)
