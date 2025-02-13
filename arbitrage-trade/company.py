ea137e22364abc82615cac82ab97717f5a1c475f
Steven <steven950811@gmail.com>
2025-02-14 00:54:31 +0800

    refactor: move stock code and future code to company

---
+++ arbitrage-trade/company.py
@@ -0,0 +1,1 @@
+company = [("3081.TWO", "OTF")]
--- arbitrage-trade/main.py
+++ arbitrage-trade/main.py
@@ -1,8 +1,10 @@
 from discord_bot.send_message import DiscordBot
+from company import company
 import os
 import dotenv
 
 dotenv.load_dotenv()
 
 dc_bot = DiscordBot(os.getenv("DCBOT_TOKEN"), os.getenv("CHANNEL_ID"))
-dc_bot.run("1101.TW", "DFF", 25)
+for stock_code, future_code in company:
+    dc_bot.run(stock_code, future_code, 25)
